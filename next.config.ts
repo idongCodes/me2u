3d185590c505c7cc0d52dcfe57b403b051dd3d0b
idongcodes <i.d.essien@gmail.com>
2026-06-05 13:39:24 -0400

    chore: setup console error handling

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['fromme2u.app', '*.fromme2u.app'],
+    },
+  },
 };
 
 export default nextConfig;
